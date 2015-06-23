38c2c3ea0ee4c7a79978beff5588f84399b086a0
Slim80 <bestboss1980@gmail.com>
2015-06-23 07:50:55 +0200

    minor changes in Imperium post boot script

--- ramfs/sbin/imperium.sh
+++ ramfs/sbin/imperium.sh
@@ -11,9 +11,13 @@ OPEN_RW()
 }
 OPEN_RW;
 
-sleep 5;
+sleep 1;
 
-sleep 5;
+# Run Qualcomm scripts
+$BB sh /sbin/init.qcom.post_boot.sh;
+
+sleep 1;
+
 OPEN_RW;
 
 # Symlink
@@ -41,54 +45,10 @@ CRITICAL_PERM_FIX()
 	$BB chmod -R 0777 /data/anr/;
 	$BB chmod -R 0400 /data/tombstones;
 	$BB chmod 06755 /sbin/busybox;
+	$BB chown -R root:root /data/property;
+	$BB chmod -R 0700 /data/property;
 }
 CRITICAL_PERM_FIX;
-
-# perm fix
-$BB chmod 666 /sys/module/lowmemorykiller/parameters/cost;
-$BB chmod 666 /sys/module/lowmemorykiller/parameters/adj;
-$BB chmod 666 /sys/module/lowmemorykiller/parameters/minfree;
-$BB chown system /sys/devices/system/cpu/cpu0/cpufreq/*;
-$BB chown system /sys/devices/system/cpu/cpu1/online;
-$BB chown system /sys/devices/system/cpu/cpu2/online;
-$BB chown system /sys/devices/system/cpu/cpu3/online;
-$BB chmod 666 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-$BB chmod 666 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-$BB chmod 666 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
-$BB chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq;
-$BB chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/stats/*;
-$BB chmod 666 /sys/devices/system/cpu/cpu1/online;
-$BB chmod 666 /sys/devices/system/cpu/cpu2/online;
-$BB chmod 666 /sys/devices/system/cpu/cpu3/online;
-$BB chmod 666 /sys/module/msm_thermal/parameters/*;
-$BB chmod 666 /sys/module/msm_thermal/core_control/enabled;
-$BB chmod 666 /sys/class/kgsl/kgsl-3d0/max_gpuclk;
-$BB chmod 666 /sys/devices/platform/kgsl-3d0/kgsl/kgsl-3d0/pwrscale/trustzone/governor;
-
-$BB chown -R root:root /data/property;
-$BB chmod -R 0700 /data/property;
-
-# Set ondemand GPU governor as default
-echo "ondemand" > /sys/devices/platform/kgsl-3d0/kgsl/kgsl-3d0/pwrscale/trustzone/governor;
-
-# Make sure our max gpu clock is set via sysfs
-echo "450000000" > /sys/class/kgsl/kgsl-3d0/max_gpuclk;
-
-# Set ondemand CPU governor as default
-echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
-echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
-echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
-
-# Set min max boot freq to default
-echo "1890000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-echo "1890000" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq;
-echo "1890000" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq;
-echo "1890000" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq;
-echo "384000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
-echo "384000" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq;
-echo "384000" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq;
-echo "384000" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq;
 
 # Prop tweaks
 setprop persist.adb.notify 0
@@ -101,12 +61,15 @@ setprop profiler.force_disable_ulog 1
 /system/xbin/daemonsu --auto-daemon &
 
 # STweaks suppot
+$BB rm -f /system/app/HybridTweaks.apk > /dev/null 2>&1;
+$BB rm -f /system/app/Hulk-Kernel sTweaks.apk > /dev/null 2>&1;
+$BB rm -f /system/app/STweaks.apk > /dev/null 2>&1;
 $BB rm -f /system/app/STweaks_Googy-Max.apk > /dev/null 2>&1;
 $BB rm -f /system/app/GTweaks.apk > /dev/null 2>&1;
 $BB rm -f /data/app/com.gokhanmoral.stweaks* > /dev/null 2>&1;
 $BB rm -f /data/data/com.gokhanmoral.stweaks*/* > /dev/null 2>&1;
-$BB chown root.root /system/app/STweaks.apk;
-$BB chmod 644 /system/app/STweaks.apk;
+$BB chown root.root /system/priv-app/STweaks.apk;
+$BB chmod 644 /system/priv-app/STweaks.apk;
 
 if [ ! -d /data/.imperium ]; then
 	$BB mkdir -p /data/.imperium;
@@ -167,9 +130,6 @@ fi;
 
 # Enable kmem interface for everyone by GM
 	echo "0" > /proc/sys/kernel/kptr_restrict;
-	
-# Run Qualcomm scripts
-$BB sh /sbin/init.qcom.post_boot.sh;
 
 # Apply STweaks defaults
 export CONFIG_BOOTING=1
