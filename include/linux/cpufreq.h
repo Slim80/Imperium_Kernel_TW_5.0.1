f88d2bfd09f92c727bd1f46d43ec7fea401ba4ad
Slim80 <bestboss1980@gmail.com>
2015-07-12 09:47:40 +0200

    Added Barry_Allen CPU governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -480,6 +480,9 @@ extern struct cpufreq_governor cpufreq_gov_preservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 
