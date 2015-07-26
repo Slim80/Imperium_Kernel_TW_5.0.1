6aed4fb7ad16f2bb0615713112bd71abe9a31af8
Slim80 <bestboss1980@gmail.com>
2015-07-26 11:33:11 +0200

    Added ArterActive CPU GOV [credits to @arter97]

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -483,6 +483,9 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ARTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_arteractive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_arteractive)
 #endif
 
 
