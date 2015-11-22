b2a1eaa43814c42ee7599285dec8fcc093eeff13
Slim80 <bestboss1980@gmail.com>
2015-11-22 10:23:54 +0100

    build_kernel: sync to the OH2 Samsung source

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,10 @@
-mkdir $(pwd)/out
-make -C $(pwd) O=$(pwd)/out VARIANT_DEFCONFIG=jf_eur_defconfig jf_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=$(pwd)/out
-cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output VARIANT_DEFCONFIG=jf_eur_defconfig jf_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
