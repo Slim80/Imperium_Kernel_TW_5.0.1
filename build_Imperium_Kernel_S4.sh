9c9b5b27a1c9092da34d79ec120ab47eaf92adbb
Slim80 <bestboss1980@gmail.com>
2017-01-21 12:12:13 +0100

    Imperium Kernel S4 v4.9

--- build_Imperium_Kernel_S4.sh
+++ build_Imperium_Kernel_S4.sh
@@ -14,7 +14,7 @@ KERNELDIR="/home/slim80/Scrivania/Kernel/Samsung/Imperium_Kernel"
 IMAGE="/home/slim80/Scrivania/Kernel/Samsung/Imperium_Kernel/arch/arm/boot"
 RAMFS="/home/slim80/Scrivania/Kernel/Samsung/Imperium_Kernel/ramfs_imperium"
 BUILDEDKERNEL="/home/slim80/Scrivania/Kernel/Samsung/Imperium_Kernel/1_Imperium"
-VERSION=4.8
+VERSION=4.9
 find -name '*.ko' -exec rm -rf {} \;
 
 rm -rf $KERNELDIR/ramfs_imperium.cpio
